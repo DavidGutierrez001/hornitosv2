ba1a81c898e77820b6040bfe26f9832180344c3a
David Gutierrez <cellzt001@gmail.com>
2026-07-17 22:25:46 -0500

    fix: responsive dimensions in banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,9 @@
-import combos from "../assets/combos.webp";
-import madre from "../assets/madre.webp";
-import panadero from "../assets/panadero.webp";
-import tarta from "../assets/tarta.webp";
-import tiramisu from "../assets/tiramisu.webp";
-import crujiente from "../assets/crujiente.webp";
+import combos1x from "../assets/banners/combos@1x.webp";
+import combos2x from "../assets/banners/combos@2x.webp";
+import madre1x from "../assets/banners/madre@1x.webp";
+import madre2x from "../assets/banners/madre@2x.webp";
+import tiramisu1x from "../assets/banners/tiramisu@1x.webp";
+import tiramisu2x from "../assets/banners/tiramisu@2x.webp";
 
 import {
     Carousel,
@@ -18,12 +18,9 @@ import Autoplay from "embla-carousel-autoplay"
 function Banner() {
 
     const banners = [
-        { src: madre, alt: "Banner 1" },
-        { src: combos, alt: "Banner 2" },
-        { src: panadero, alt: "Banner 3" },
-        { src: tarta, alt: "Banner 4" },
-        { src: tiramisu, alt: "Banner 5" },
-        { src: crujiente, alt: "Banner 6" }
+        { src: madre1x, srcSet: `${madre1x} 1x, ${madre2x} 2x`, alt: "Banner 1" },
+        { src: combos1x, srcSet: `${combos1x} 1x, ${combos2x} 2x`, alt: "Banner 2" },
+        { src: tiramisu1x, srcSet: `${tiramisu1x} 1x, ${tiramisu2x} 2x`, alt: "Banner 4" },
     ]
 
     return (
@@ -40,18 +37,21 @@ function Banner() {
             >
                 <CarouselContent className="relative w-[calc(100vw+3rem)]">
                     {banners.map((banner, index) => (
-                        <CarouselItem key={index} className="h-60 lg:h-[calc(100svh-68px-250px)] flex items-center justify-center lg:px-15">
+                        <CarouselItem key={index} className="h-auto lg:h-[calc(100svh-68px-250px)] flex items-center justify-center lg:px-15">
                             <img
                                 className="md:h-fit object-cover w-full max-w-11/12 cursor-pointer lg:rounded-[5rem] shadow"
                                 src={banner.src}
+                                srcSet={`${banner.srcSet || undefined}`}
+                                sizes="(max-width: 1024px) 100vw, (max-width: 1200px) 90vw, 80vw"
                                 alt={banner.alt}
                                 fetchPriority="high"
+                                decoding="async"
                             />
                         </CarouselItem>
                     ))}
                 </CarouselContent>
-                <CarouselPrevious className="absolute left-5 top-43 md:top-0 h-10 w-10 md:h-15 md:w-15 bg-(--white) cursor-pointer shadow-xl hover:bg-white border-0" />
-                <CarouselNext className="absolute right-5 top-43 md:top-0 h-10 w-10 md:h-15 md:w-15 bg-(--white) cursor-pointer shadow-xl hover:bg-white border-0" />
+                <CarouselPrevious className="absolute left-5 md:top-0 h-10 w-10 md:h-15 md:w-15 bg-(--white) cursor-pointer shadow-xl hover:bg-white border-0" />
+                <CarouselNext className="absolute right-5 md:top-0 h-10 w-10 md:h-15 md:w-15 bg-(--white) cursor-pointer shadow-xl hover:bg-white border-0" />
             </Carousel>
         </div>
     )
