354d9f41205e9cc71b731c9805c07b6ac9276f3e
David Gutierrez <cellzt001@gmail.com>
2026-07-19 23:59:57 -0500

    fix: desktop dimensiones on banner

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,9 +1,9 @@
-import combos1x from "../../assets/banners/combos@1x.webp";
-import combos2x from "../../assets/banners/combos@2x.webp";
-import madre1x from "../../assets/banners/madre@1x.webp";
-import madre2x from "../../assets/banners/madre@2x.webp";
-import tiramisu1x from "../../assets/banners/tiramisu@1x.webp";
-import tiramisu2x from "../../assets/banners/tiramisu@2x.webp";
+import combosDesktop from "../../assets/banners/combos@1x.webp";
+import combosMobile from "../../assets/banners/combos@2x.webp";
+import madreDesktop from "../../assets/banners/madre@1x.webp";
+import madreMobile from "../../assets/banners/madre@2x.webp";
+import tiramisuDesktop from "../../assets/banners/tiramisu@1x.webp";
+import tiramisuMobile from "../../assets/banners/tiramisu@2x.webp";
 
 import {
     Carousel,
@@ -13,48 +13,51 @@ import {
     CarouselPrevious,
 } from "@/components/ui/carousel";
 
-import Autoplay from "embla-carousel-autoplay"
+import Autoplay from "embla-carousel-autoplay";
 
 function Banner() {
-
     const banners = [
-        { src: madre1x, srcSet: `${madre1x} 1x, ${madre2x} 2x`, alt: "Banner 1" },
-        { src: combos1x, srcSet: `${combos1x} 1x, ${combos2x} 2x`, alt: "Banner 2" },
-        { src: tiramisu1x, srcSet: `${tiramisu1x} 1x, ${tiramisu2x} 2x`, alt: "Banner 4" },
-    ]
+        { desktop: combosDesktop, mobile: combosMobile, alt: "Banner 2" },
+        { desktop: madreDesktop, mobile: madreMobile, alt: "Banner 1" },
+        { desktop: tiramisuDesktop, mobile: tiramisuMobile, alt: "Banner 3" },
+    ];
 
     return (
-        <div className="w-full relative bg-banner">
+        <div className="relative w-full bg-banner">
             <Carousel
-                opts={{
-                    loop: true,
-                }}
+                opts={{ loop: true }}
                 plugins={[
                     Autoplay({
                         delay: 5000,
                     }),
                 ]}
+                className="w-full"
             >
-                <CarouselContent className="relative w-[calc(100vw+3rem)]">
+                <CarouselContent>
                     {banners.map((banner, index) => (
-                        <CarouselItem key={index} className="h-auto lg:h-[calc(100svh-68px-250px)] flex items-center justify-center lg:px-15">
-                            <img
-                                className="md:h-fit object-cover w-full max-w-11/12 cursor-pointer lg:rounded-[5rem] shadow"
-                                src={banner.src}
-                                srcSet={`${banner.srcSet || undefined}`}
-                                sizes="(max-width: 1024px) 100vw, (max-width: 1200px) 90vw, 80vw"
-                                alt={banner.alt}
-                                fetchPriority="high"
-                                decoding="async"
-                            />
+                        <CarouselItem
+                            key={index}
+                            className="h-auto w-full md:py-10 object-cover flex items-center justify-center"
+                        >
+                            <picture className="w-full flex justify-center">
+                                <source media="(min-width: 640px)" srcSet={banner.desktop} />
+                                <img
+                                    src={banner.mobile}
+                                    alt={banner.alt}
+                                    className="md:h-fit w-full cursor-pointer shadow"
+                                    fetchPriority="high"
+                                    decoding="async"
+                                />
+                            </picture>
                         </CarouselItem>
                     ))}
                 </CarouselContent>
-                <CarouselPrevious className="absolute left-5 md:top-0 h-10 w-10 md:h-15 md:w-15 bg-(--white) cursor-pointer shadow-xl hover:bg-white border-0" />
-                <CarouselNext className="absolute right-5 md:top-0 h-10 w-10 md:h-15 md:w-15 bg-(--white) cursor-pointer shadow-xl hover:bg-white border-0" />
+
+                <CarouselPrevious className="absolute cursor-pointer left-2 h-9 w-9 md:left-6 md:h-15 md:w-15 border-0 bg-white shadow-lg hover:bg-white" />
+                <CarouselNext className="absolute cursor-pointer right-2 h-9 w-9 md:right-6 md:h-15 md:w-15 border-0 bg-white shadow-lg hover:bg-white" />
             </Carousel>
         </div>
-    )
+    );
 }
 
-export default Banner
+export default Banner;
--- src/pages/Home/MenuSection.jsx
+++ src/pages/Home/MenuSection.jsx
@@ -12,35 +12,37 @@ import milhoja from "../../assets/menu/milhoja.webp"
 import granola from "../../assets/menu/granola.webp"
 
 import { Link } from "react-router-dom"
+import { act } from "react"
 
 function MenuSection() {
 
     const menuItems = [
-        { name: "Lanzamientos", url: lanzamientos },
-        { name: "Promociones", url: promociones },
-        { name: "Desayunos", url: calentado },
-        { name: "Tardes Santafereñas", url: chocolate },
-        { name: "Para seguir el día", url: bruschetta },
-        { name: "Acompañamientos", url: bunuelo },
-        { name: "Antojos", url: corazoncitos },
-        { name: "Tortas y Ponqués", url: torta },
-        { name: "Panes para la mesa", url: pan },
-        { name: "Panes para ensamble", url: pan2 },
-        { name: "Repostería", url: milhoja },
-        { name: "La alacena", url: granola },
+        { name: "Lanzamientos", url: lanzamientos, activeElement: true },
+        { name: "Promociones", url: promociones, activeElement: true },
+        { name: "Desayunos", url: calentado, activeElement: false },
+        { name: "Tardes Santafereñas", url: chocolate, activeElement: false },
+        { name: "Para seguir el día", url: bruschetta, activeElement: false },
+        { name: "Acompañamientos", url: bunuelo, activeElement: false },
+        { name: "Antojos", url: corazoncitos, activeElement: false },
+        { name: "Tortas y Ponqués", url: torta, activeElement: false },
+        { name: "Panes para la mesa", url: pan, activeElement: false },
+        { name: "Panes para ensamble", url: pan2, activeElement: false },
+        { name: "Repostería", url: milhoja, activeElement: false },
+        { name: "La alacena", url: granola, activeElement: false },
     ]
 
     return (
-        <section id="menu" className="flex flex-col items-center px-3 py-10 ">
+        <section id="menu" className="flex flex-col items-center px-3">
             <div className="max-w-7xl">
                 <h1 className="text-[clamp(2rem,4vw,3rem)] flex items-center justify-center md:justify-start w-full pt-5 pb-10 md:pt-20 md:pb-10 uppercase text-(--orange-dark) font-cinzel">Nuestro Menú</h1>
-                <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 w-full gap-3 md:gap-5">
+                <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 w-full gap-3 md:gap-3">
                     {menuItems.map((item, index) => (
                         <div className="overflow-hidden">
                             <Link
                                 to={'/menu/' + item.name.toLowerCase()}
                                 key={index}
-                                className="relative w-full cursor-pointer rounded-sm opacity-90 hover:opacity-100 transition-opacity duration-300"
+                                className={`relative w-full cursor-pointer rounded-sm opacity-90 hover:opacity-100 transition-opacity duration-300
+                                    ${item.activeElement ? '' : 'grayscale-90 pointer-events-none hover:cursor-not-allowed'}`}
                                 onClick={() => {
                                     setTimeout(() => {
                                         window.scrollTo({
@@ -51,9 +53,11 @@ function MenuSection() {
                             >
 
                                 <img
-                                    className="w-fit h-50 sm:h-fit object-cover rounded hover:scale-105 transition-transform duration-300" src={item.url} alt={item.name}
+                                    className="w-fit h-50 sm:h-fit object-contain rounded hover:scale-105 transition-transform duration-300"
+                                    src={item.url}
+                                    alt={item.name}
                                 />
-                                <footer className="bg-(--brown) h-10 sm:h-15 flex items-center justify-center rounded-b text-white font-semibold tracking-wider uppercase absolute bottom-0 w-full">
+                                <footer className="bg-(--brown)/90 h-10 sm:h-15 flex items-center justify-center rounded-b text-white font-semibold tracking-wider uppercase absolute bottom-0 w-full">
                                     <span className="text-xs md:text-lg text-center">{item.name}</span>
                                 </footer>
                             </Link>
